18389175fbf262b7acca877dead3dcdfe90e2ce0
pemudapeduliwarga <riskiprtamaa58@gmail.com>
2026-09-17 23:53:17 +0700

    perf(public): isolate dynamic auth routes

---
+++ app/(public)/login/layout.tsx
@@ -0,0 +1,14 @@
+export const dynamic =
+  "force-dynamic";
+
+export default function DynamicPublicSectionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
